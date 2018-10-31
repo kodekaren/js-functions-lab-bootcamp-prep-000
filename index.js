341f9674c5e4317c4f920ad7ec4821b3a8c7aaff
karen ercoli <karenercoli@gmail.com>
2018-10-31 12:28:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 
 function happyHolidays() {
-  return "Happy Holidays!"
+  return ("Happy Holidays!")
 }
 happyHolidays()
 
 function happyHolidaysTo(name) {
   return string("Happy holidays to ""${name}"" !")
 }
+happyHolisaysTo(Karen)
+happyHolisaysTo(Davide)
 
 function happyHolidayTo(holiday,name) {
   return string(`Happy ${holiday}, ${name}!`)
+
 }
+happyHolidayTo(Christmas, Karen)
+  
 function holidayCountdown(holiday,days) {
   return string(`It's ${days} days until ${holiday}!`)
 }
