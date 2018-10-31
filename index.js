697efb58b7d3070442f30ff71642ad944bde0a65
karen ercoli <karenercoli@gmail.com>
2018-10-31 11:57:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// write your code below!
+function happyHolidays() {
+  return happyHolidays("Happy holidays")
+}// write your code below!
+function happyHolidaysTo(name) {
+  return string("Happy holidays to ""${name}"" !")
+}
